a80414d8ba74ba668f40acf7393e12e6f1167d1d
Caleb Tony <calebmbugua@gmail.com>
2021-08-30 10:56:24 +0300

    added functionality to close sidebar after a link has been clicked

--- src/app/components/global/sidebar/sidebar.component.ts
+++ src/app/components/global/sidebar/sidebar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/services/auth/auth-service.service';
+import { SidebarService } from 'src/app/services/sidebar.service';
 
 @Component({
   selector: 'app-sidebar',
@@ -7,9 +8,16 @@ import { AuthService } from 'src/app/services/auth/auth-service.service';
   styleUrls: ['./sidebar.component.css'],
 })
 export class SidebarComponent implements OnInit {
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService,
+    private sidebarService: SidebarService
+  ) {}
 
   ngOnInit(): void {}
+
+  closeSidebar() {
+    this.sidebarService.closeSidebar();
+  }
 
   logout() {
     this.authService.logout();
