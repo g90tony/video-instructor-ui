a6c62297f5919b9e662e8ff7b3abb5cfe2be7df5
Caleb Tony <calebmbugua@gmail.com>
2021-09-02 09:06:40 +0300

    added search functionality

--- src/app/pages/registered-search/registered-search.component.ts
+++ src/app/pages/registered-search/registered-search.component.ts
@@ -1,14 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { RegisteredCoursesService } from 'src/app/services/registered-course/registered-course.service';
 
 @Component({
   templateUrl: './registered-search.component.html',
-  styleUrls: ['./registered-search.component.css']
+  styleUrls: ['./registered-search.component.css'],
 })
 export class RegisteredSearchComponent implements OnInit {
+  search_query: string = '';
+  search_results: any;
+  search_results_count: number = 0;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private route: ActivatedRoute,
+    private registeredCoursesService: RegisteredCoursesService
+  ) {
+    this.route.params.subscribe((params) => {
+      this.search_query = params['search_query'];
+    });
   }
 
+  ngOnInit(): void {
+    this.registeredCoursesService
+      .get_search_query(this.search_query)
+      .subscribe((res) => {
+        console.log(res);
+        this.search_results = res;
+        this.search_results_count = res.length;
+      });
+  }
 }
--- src/app/pages/registered/registered.component.ts
+++ src/app/pages/registered/registered.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 import { RegisteredCoursesService } from 'src/app/services/registered-course/registered-course.service';
 import { UserProfileService } from 'src/app/services/user-profile/user-profile.service';
 
@@ -10,9 +12,14 @@ import { UserProfileService } from 'src/app/services/user-profile/user-profile.s
 export class RegisteredComponent implements OnInit {
   registered_courses: any[];
 
+  searchForm = new FormGroup({
+    search: new FormControl(''),
+  });
+
   constructor(
     private registeredCourseService: RegisteredCoursesService,
-    private userProfileService: UserProfileService
+    private userProfileService: UserProfileService,
+    private router: Router
   ) {
     this.registered_courses = [];
   }
@@ -23,5 +30,11 @@ export class RegisteredComponent implements OnInit {
     this.registeredCourseService.get_registered_courses().subscribe((res) => {
       this.registered_courses = res;
     });
+  }
+
+  onSubmit() {
+    const form_data = this.searchForm.value;
+
+    this.router.navigateByUrl(`/courses/registered/search/${form_data.search}`);
   }
 }
